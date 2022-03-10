1099356d49e9361ef3c77798ccc03787a120ea5b
irp2605 <irp2605@gmail.com>
2022-03-10 11:02:19 -0600

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is just a test for github");
+        System.out.println("This is just the test for github");
     }
 }
