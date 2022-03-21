ea483e7bf69b0df422112c846d1bda225dbbd999
irp2605 <irp2605@gmail.com>
2022-03-21 11:19:24 -0500

    Getters and Setters for Player (fr this time be done pls)

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,46 @@
 import java.awt.*;
 
 public class Player {
+    public int getGridX() {
+        return gridX;
+    }
+
+    public void setGridX(int gridX) {
+        this.gridX = gridX;
+    }
+
+    public int getGridY() {
+        return gridY;
+    }
+
+    public void setGridY(int gridY) {
+        this.gridY = gridY;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Rectangle hitbox) {
+        this.hitbox = hitbox;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
     private int gridX;
     private int gridY;
     private Rectangle hitbox;
