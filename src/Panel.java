640f896777c4912572b230a10f0b6042fb6cdafd
irp2605 <irp2605@gmail.com>
2022-03-23 11:25:01 -0500

    Starting off the world drawing

--- src/Panel.java
+++ src/Panel.java
@@ -1,11 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Panel extends JPanel implements KeyListener, Runnable {
+    private int currWorld =1;
+    private Player player;
+    private World world;
+    private ArrayList<World> worlds;
+    private int updatesPerSecond = 30;
+    private Thread t;
+    public Panel() {
+        addKeyListener(this);
+        worlds.add(new World(new File("test.txt")));
+        world = worlds.get(1);
+        world.textConverter();
+    }
+    public void Paint(Graphics g) {
+        for(int c=0; c<world.getNonplayerList().size(); c++) {
+            if(world.getNonplayerList().get(c).getType()==0) {
+                g.setColor(Color.lightGray);
+                g.drawRect(world.getNonplayerList().get(c).getGridX()*10, world.getNonplayerList().get(c).getGridX()*10, 10, 10);
+            }
 
+        }
+    }
 
+    public void update() {
 
+    }
+
+    public void reset() {
+
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
--- src/World.java
+++ src/World.java
@@ -1,2 +1,41 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class World {
+    public ArrayList<NonPlayer> getNonplayerList() {
+        return NonplayerList;
+    }
+
+    private ArrayList<NonPlayer> NonplayerList;
+    private File f;
+    private Scanner fileReader;
+    public World(File f) {
+        try {
+            this.f = f;
+            fileReader = new Scanner(f);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void textConverter() {
+        try{
+            int curr = -1;
+            int c=0;
+            while(fileReader.hasNextInt()) {
+                curr=fileReader.nextInt();
+                c++;
+                if(curr==0) {
+                    NonplayerList.add(new NonPlayer(0, 0, c%10, c/10, 0, 10, 10));
+                }
+                if(curr==4) {
+                    NonplayerList.add(new Wall(0, Constants.WALL, c%10, c/10, 0, 10, 10));
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
