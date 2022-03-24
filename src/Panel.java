4f48cc91bafe7bd13522c5967cb91090a6bc0a7d
irp2605 <irp2605@gmail.com>
2022-03-24 10:47:44 -0500

    got the player spawning at the player spawn

--- src/Panel.java
+++ src/Panel.java
@@ -17,17 +17,26 @@ public class Panel extends JPanel implements KeyListener, Runnable {
         addKeyListener(this);
         worlds = new ArrayList<World>();
         try {
-            worlds.add(new World(new File("C:\\Users\\irp26\\Documents\\CS3\\DodgeGame\\src\\test.txt")));
+            worlds.add(new World(new File("C:\\Users\\k1403420\\IdeaProjects\\DodgeGame\\src\\test.txt")));
             world = worlds.get(0);
             world.textConverter();
             System.out.println(world.getNonplayerList().toString());
+            player = new Player(-1,-1,0, 3);
+            for(int c=0; c<world.getNonplayerList().size(); c++) {
+                if(world.getNonplayerList().get(c).getType()== Constants.PLAYER) {
+                    player.setGridX(world.getNonplayerList().get(c).getGridX());
+                    player.setGridY(world.getNonplayerList().get(c).getGridY());
+                }
+            }
+            player.setHitbox(new Rectangle(player.getGridX()*100, player.getGridY()*100, 50, 50));
+            System.out.println(player.getHitbox().getX() + " " + player.getHitbox().getY());
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+
     }
     public void paint(Graphics g) {
-        System.out.println("W");
         g.setFont(new Font("Times New Roman", Font.BOLD, 25));
         for(int c=0; c<world.getNonplayerList().size(); c++) {
             System.out.println( world.getNonplayerList().get(c).getType() + ":" + world.getNonplayerList().get(c).getGridX() + "," + world.getNonplayerList().get(c).getGridY());
@@ -52,6 +61,8 @@ public class Panel extends JPanel implements KeyListener, Runnable {
             g.drawRect(world.getNonplayerList().get(c).getGridX()*100, world.getNonplayerList().get(c).getGridY()*100, 100, 100);
             g.drawString(String.valueOf(c),world.getNonplayerList().get(c).getGridX()*100, world.getNonplayerList().get(c).getGridY()*100+100);
         }
+        g.setColor(Color.pink);
+        g.fillOval((int)player.getHitbox().getX(), (int)player.getHitbox().getY(), 50, 50);
     }
 
     public void update() {
